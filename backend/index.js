1b9919ebf2e42dbb47d2d09fc784a8814f422014
Harshil Tanwar <harshiltanwar62@gmail.com>
2024-07-25 10:55:57 +0530

    trycatch

--- backend/index.js
+++ backend/index.js
@@ -7,12 +7,12 @@ import 'dotenv/config';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect(process.env.DB_URL).then(() => {
+try {
+	mongoose.connect(process.env.DB_URL);
   console.log('MongoDB connected');
-}).catch(err => {
+} catch (err) {
   console.error('MongoDB connection error:', err);
-  process.exit(1);
-});
+}
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
